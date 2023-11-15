ee0da539b553740b24b7f19df4b04473c9d66722
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-11-14 21:19:18 -0500

    added songRoutes function for GET requests and fixed songRevRoutes GET request

--- controllers/api/songRevRoutes.js
+++ controllers/api/songRevRoutes.js
@@ -2,15 +2,13 @@ const router = require('express').Router();
 const { User, SongReview, AlbumReview, Song, Album, Artist } = require('../../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const songRevData = SongReview.findAll({
+        SongReview.findAll({
             attributes: [
                 'id',
                 'title',
                 'review',
                 'song_id'
             ],
-            order: ['created_at', 'DESC'],
             include: [
                 {
                     model: User,
@@ -31,14 +29,13 @@ router.get('/', async (req, res) => {
                     ]
                 }
             ]
-        });
-        if (!songRevData) {
-            res.status(404).json({ message: 'No data found!' });
-        }
-        res.json(songRevData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+        })
+        .then(songRevData => res.json(songRevData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    
 });
 
 module.exports = router;
