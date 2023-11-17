dd576184945d083dcfc3306b77123009d7ff3bf0
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-11-16 21:11:10 -0500

    logging in and out now work properly

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+async function loginFormHandler(e) {
+    e.preventDefault();
+    const name = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (name && password) {
+        const res = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (res.ok) {
+            document.location.replace('/');
+            alert('good!')
+        } else {
+            alert(res.statusText);
+        }
+    }
+}
+
+document.getElementById('login-form').addEventListener('submit', loginFormHandler);
